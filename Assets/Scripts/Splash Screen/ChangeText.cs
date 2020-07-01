7706b2ef2114b4e7046b556199a8964b50046272
Green-Blod <bloodjey98@gmail.com>
2020-07-01 14:56:32 +0500

    Initial commit

---
+++ Assets/Scripts/Splash Screen/ChangeText.cs
@@ -0,0 +1,46 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Splash_Screen
+{
+    public class ChangeText : MonoBehaviour
+    {
+        public Text[] splashScreenText;
+        //public TMP_Text[] infoScreenText;
+        public string[] uzbekText, russianText; 
+        
+        public void ChangeToUzbekSplash()
+        {
+            for (var i = 0; i < splashScreenText.Length; i++)
+            {
+                splashScreenText[i].text = uzbekText[i];
+            }
+        }
+
+        public void ChangeToRussianSplash()
+        {
+            for (var i = 0; i < splashScreenText.Length; i++)
+            {
+                splashScreenText[i].text = russianText[i];
+            }
+        }
+        
+        
+        /*public void ChangeToUzbekInfo()
+        {
+            for (var i = 0; i < infoScreenText.Length; i++)
+            {
+                infoScreenText[i].text = uzbekText[i];
+            }
+        }
+
+        public void ChangeToRussianInfo()
+        {
+            for (var i = 0; i < infoScreenText.Length; i++)
+            {
+                infoScreenText[i].text = russianText[i];
+            }
+        }*/
+    }
+}
