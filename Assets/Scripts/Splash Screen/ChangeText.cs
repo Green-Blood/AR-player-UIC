8dd549af9177c671d89de53cd94d267c2b1212cf
Green-Blod <bloodjey98@gmail.com>
2020-07-01 18:01:53 +0500

    Last

--- Assets/Scripts/Splash Screen/ChangeText.cs
+++ Assets/Scripts/Splash Screen/ChangeText.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,10 +9,44 @@ namespace Splash_Screen
     {
         public Text[] splashScreenText;
         //public TMP_Text[] infoScreenText;
-        public string[] uzbekText, russianText; 
-        
+        public string[] uzbekText, russianText;
+
+        public Button uzbekBtn, russianBtn;
+        Sprite ButtonOn;
+        Sprite ButtonOff;
+        private bool _isClicked;
+        private Color uzbekTextColor, russianTextColor;
+
+        private void Start()
+        {
+            ButtonOn = uzbekBtn.image.sprite;
+            ButtonOff = russianBtn.image.sprite;
+            
+            uzbekTextColor = uzbekBtn.GetComponentInChildren<TextMeshProUGUI>().color;
+            russianTextColor = russianBtn.GetComponentInChildren<TextMeshProUGUI>().color;
+        }
+
         public void ChangeToUzbekSplash()
         {
+
+            _isClicked = true;
+            if (_isClicked)
+            {
+            
+                uzbekBtn.image.overrideSprite = ButtonOn;
+                russianBtn.image.overrideSprite = ButtonOff;
+                uzbekBtn.GetComponentInChildren<TextMeshProUGUI>().color = uzbekTextColor;
+                russianBtn.GetComponentInChildren<TextMeshProUGUI>().color = russianTextColor;
+            }
+            else
+            {
+            
+                uzbekBtn.image.overrideSprite = ButtonOff;
+                russianBtn.image.overrideSprite = ButtonOn;
+                uzbekBtn.GetComponentInChildren<TextMeshProUGUI>().color = russianTextColor;
+                russianBtn.GetComponentInChildren<TextMeshProUGUI>().color = uzbekTextColor;
+            }
+            
             for (var i = 0; i < splashScreenText.Length; i++)
             {
                 splashScreenText[i].text = uzbekText[i];
@@ -20,6 +55,21 @@ namespace Splash_Screen
 
         public void ChangeToRussianSplash()
         {
+            _isClicked = false;
+            if (_isClicked)
+            {
+                uzbekBtn.GetComponentInChildren<TextMeshProUGUI>().color = uzbekTextColor;
+                russianBtn.GetComponentInChildren<TextMeshProUGUI>().color = russianTextColor;
+                uzbekBtn.image.overrideSprite = ButtonOn;
+                russianBtn.image.overrideSprite = ButtonOff;
+            }
+            else
+            {
+                uzbekBtn.GetComponentInChildren<TextMeshProUGUI>().color = russianTextColor;
+                russianBtn.GetComponentInChildren<TextMeshProUGUI>().color = uzbekTextColor;
+                uzbekBtn.image.overrideSprite = ButtonOff;
+                russianBtn.image.overrideSprite = ButtonOn;
+            }
             for (var i = 0; i < splashScreenText.Length; i++)
             {
                 splashScreenText[i].text = russianText[i];
